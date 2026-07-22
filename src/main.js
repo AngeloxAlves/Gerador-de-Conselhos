12d22dc894fed9fbe5b138e5582583313916dc47
Angelo Alves <angeloalves1580@gmail.com>
2026-07-22 19:12:44 -0300

    Primeiras codagens de lógica JS

--- src/main.js
+++ src/main.js
@@ -1,3 +1,24 @@
-const btn = document.getElementById("btn-generate");
+import api from "./api.js";
 
-btn.addEventListener("click", showAdvice());
+const btn = document.getElementById("btn-generate");
+const advice = document.getElementById("advice-text");
+
+btn.addEventListener("click", showAdvice);
+
+async function showAdvice() {
+  try {
+    btn.disabled = true;
+    btn.textContent = "Carregando...";
+
+    const data = await api.buscarConselho();
+
+    advice.textContent = `"${data.slip.advice}"`;
+    advice.classList.remove("hidden");
+  } catch {
+    advice.textContent = "Não foi possível carregar um conselho no momento.";
+    advice.classList.remove("hidden");
+  } finally {
+    btn.disabled = false;
+    btn.textContent = "Gerar conselho";
+  }
+}
